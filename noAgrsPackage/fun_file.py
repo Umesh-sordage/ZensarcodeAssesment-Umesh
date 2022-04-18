7aa394b3cc13218ad10770e78e4d91736e341254
Umesh-sordage <103917125+Umesh-sordage@users.noreply.github.com>
2022-04-18 19:56:11 +0530

    Add files via upload

---
+++ noAgrsPackage/fun_file.py
@@ -0,0 +1,19 @@
+def begin_end(fun):
+    def inner(x, y):
+        fun(x, y)
+
+    return inner
+
+
+def dec_with_arg(name1, name2, name3):
+    print(name1, name2, name3)
+
+    def function(func):
+        def inner(*args):
+            print("Welcome.....")
+            func(*args)
+            print("End.....")
+
+        return inner
+
+    return function
